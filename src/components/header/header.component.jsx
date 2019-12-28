1889113a5796fc4ec79e6569ec5e1d984bf1e8f5
varunkverma <varunkverma1996@gmail.com>
2019-12-28 16:39:43 +0530

    Included react redux to set currentUser in the reducer and made app and header component independent of state drilling

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -3,6 +3,8 @@ import { Link } from "react-router-dom";
 import { auth } from "../firebase/firebase.utils";
 import { ReactComponent as Logo } from "../../assets/crown.svg";
 
+import { connect } from "react-redux";
+
 import "./header.styles.scss";
 
 const Header = ({ currentUser }) => (
@@ -30,4 +32,8 @@ const Header = ({ currentUser }) => (
   </div>
 );
 
-export default Header;
+// this state is the root reducer state
+const mapStateToProps = state => ({
+  currentUser: state.user.currentUser
+});
+export default connect(mapStateToProps)(Header);
