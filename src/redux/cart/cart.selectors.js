fb08a44b3556822bf2d2d69e88b8c6cb3541941e
varunkverma <varunkverma1996@gmail.com>
2019-12-29 22:38:46 +0530

    added features of reducer selectors to create memoizedselectors to increase the effciency of mapStateToProps performance

---
+++ src/redux/cart/cart.selectors.js
@@ -0,0 +1,19 @@
+import { createSelector } from "reselect";
+
+// input selector, (doesn't require reselect)
+const selectCart = state => state.cart;
+
+// output selector, (craeted using input selectors and createSelector). Since a createSelector is used, its called a memoized selector
+export const selectCartItems = createSelector(
+  [selectCart],
+  cart => cart.cartItems
+);
+
+export const selectCartItemsCount = createSelector(
+  [selectCartItems],
+  cartItems =>
+    cartItems.reduce(
+      (totalQuantity, item) => (totalQuantity += item.quantity),
+      0
+    )
+);
