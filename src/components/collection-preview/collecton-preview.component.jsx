50ce8abd1fd838f9913f62f093b934fcc63142e5
varunkverma <varunkverma1996@gmail.com>
2020-01-05 00:22:59 +0530

    added a collection-overview component to display collections of items which was done previously by the shop component. Shop component now renders collections-overview

--- src/components/collection-preview/collecton-preview.component.jsx
+++ src/components/collection-preview/collecton-preview.component.jsx
@@ -7,7 +7,6 @@ const CollectionPreview = ({ title, items }) => (
     <h2 className="title">{title.toUpperCase()}</h2>
     <div className="preview">
       {items.slice(0, 4).map(item => (
-        // <div key={item.id}>{item.name}</div>
         <CollectionItem key={item.id} item={item} />
       ))}
     </div>
