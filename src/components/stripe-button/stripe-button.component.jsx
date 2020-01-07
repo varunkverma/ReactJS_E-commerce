241376f7d5300799e23667a7d1064bf32f6cc892
Varun Verma <varunkverma@gmail.com>
2020-01-08 00:28:01 +0530

    masking stripe's publishable key

--- src/components/stripe-button/stripe-button.component.jsx
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -3,7 +3,7 @@ import StripeCheckout from "react-stripe-checkout";
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
-  const publishableKey = "pk_test_2e5rLwgHaoMhl5UHguGbOBNn006pu6wu3C";
+  const publishableKey = "Your stripe publishable key";
 
   const onToken = token => {
     console.log(token);
